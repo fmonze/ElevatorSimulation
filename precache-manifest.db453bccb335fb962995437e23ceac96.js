e201441b152ea6a083415be5a5b08a8d8538b158
Francesca <francimn_95@hotmail.com>
2019-03-12 07:32:39 +0100

    Updates

---
+++ precache-manifest.db453bccb335fb962995437e23ceac96.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f24fdf254948b949d11fb584e3bc7086",
+    "url": "/elevatorSimulation/static/media/b2.f24fdf25.svg"
+  },
+  {
+    "revision": "8603bea3f33ac0f8787c",
+    "url": "/elevatorSimulation/static/css/main.9df15ac3.chunk.css"
+  },
+  {
+    "revision": "372f78d136149aeb5ea2",
+    "url": "/elevatorSimulation/static/js/runtime~main.372f78d1.js"
+  },
+  {
+    "revision": "54a0bb59174ea0386ed555ae403e903b",
+    "url": "/elevatorSimulation/static/media/open_ elevator_img.54a0bb59.jpg"
+  },
+  {
+    "revision": "7594b8d3fc3e04c0e297",
+    "url": "/elevatorSimulation/static/js/2.7594b8d3.chunk.js"
+  },
+  {
+    "revision": "047235eda57148f199eb11b219bf749e",
+    "url": "/elevatorSimulation/static/media/current_floor_button.047235ed.svg"
+  },
+  {
+    "revision": "a5c7428c3c775394adc12a4a40005435",
+    "url": "/elevatorSimulation/static/media/closed_cage.a5c7428c.svg"
+  },
+  {
+    "revision": "0cfbf59783acd8fe8fe31f9bca1fe0dc",
+    "url": "/elevatorSimulation/static/media/b0.0cfbf597.svg"
+  },
+  {
+    "revision": "9477a1aa4d1354d7f71e990ea7698f2d",
+    "url": "/elevatorSimulation/static/media/b1.9477a1aa.svg"
+  },
+  {
+    "revision": "8603bea3f33ac0f8787c",
+    "url": "/elevatorSimulation/static/js/main.8603bea3.chunk.js"
+  },
+  {
+    "revision": "41db1cc0d20236e0ef339a9ca0cd3323",
+    "url": "/elevatorSimulation/static/media/b3.41db1cc0.svg"
+  },
+  {
+    "revision": "706df9c4d3a65b20231b45a2747f60f3",
+    "url": "/elevatorSimulation/static/media/b4.706df9c4.svg"
+  },
+  {
+    "revision": "1b8dbff6e1804f8a3616d0e0a257cfff",
+    "url": "/elevatorSimulation/static/media/b5.1b8dbff6.svg"
+  },
+  {
+    "revision": "160db443ff2a27c2927f08eb2039f488",
+    "url": "/elevatorSimulation/static/media/open_cage.160db443.svg"
+  },
+  {
+    "revision": "36ee2498dbd97886e85f0a2214830e9e",
+    "url": "/elevatorSimulation/static/media/upButton.36ee2498.svg"
+  },
+  {
+    "revision": "02afb3dad9795551e20f589f37e961a0",
+    "url": "/elevatorSimulation/static/media/downButton.02afb3da.svg"
+  },
+  {
+    "revision": "7594b8d3fc3e04c0e297",
+    "url": "/elevatorSimulation/static/css/2.1f1d75de.chunk.css"
+  },
+  {
+    "revision": "a0e7cf18f16f4f02d461f388491d3904",
+    "url": "/elevatorSimulation/index.html"
+  }
+];
